dd37c03b597bb78d871d30762661cc7059f83250
Arturo Gamez <arturogamez51@gmail.com>
2025-08-07 22:04:27 -0700

    doing something to my not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link';
+import { Suspense } from 'react';
 import Header from '@/components/landing/Header';
 import Footer from '@/components/landing/Footer';
 
 export default function NotFound() {
     return (
         <>
-            <Header />
+            <Suspense>
+                <Header />
+            </Suspense>
             <main className="flex flex-col items-center justify-center text-center bg-gray-50 py-20 sm:py-40">
                 <h1 className="text-6xl font-bold text-orange-500">404</h1>
                 <h2 className="mt-4 text-3xl font-bold text-gray-900">
@@ -21,7 +24,9 @@ export default function NotFound() {
                     Go Back Home
                 </Link>
             </main>
-            <Footer />
+            <Suspense>
+                <Footer />
+            </Suspense>
         </>
     );
 }
